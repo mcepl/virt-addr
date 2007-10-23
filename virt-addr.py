f382dd07a88fdef92bde292772ea85604d4eed65
Matej Cepl <mcepl@redhat.com>
2007-10-23 17:32:08 +0200

    Fix indexes in parsing the /var/log/messages.

--- virt-addr.py
+++ virt-addr.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 
 from lxml import etree
 import libvirt
 import sys,os
 import StringIO
 
+debug = False
 parser = etree.XMLParser(remove_blank_text=True)
 
 def macAddrFromdomObj(domObj):
@@ -20,8 +23,10 @@ if conn == None:
     sys.exit(1)
 
 domains=conn.listDomainsID()
+if debug:
+    print >>sys.stderr,"domains = %s" % domains
 
-messages=os.popen("grep 'dnsmasq.*DHCPACK' /var/log/messages","r").readlines()
+messages=os.popen("grep 'dnsmasq.*DHCPACK' /var/log/daemon.log","r").readlines()
 messages.reverse()
 
 for domx in domains:
@@ -31,11 +36,17 @@ for domx in domains:
         print 'Failed to find the domain with id=%d' % domx
         sys.exit(1)
     macaddr=macAddrFromdomObj(domCurr)
+    if debug:
+        print >>sys.stderr,"macaddr = %s" % macaddr
     ipData = []
     for lineStr in messages:
         line = lineStr.strip().split()
+        if debug:
+            print >>sys.stderr,"line = %s" % line
         if line[-1]==macaddr:
             ipData=line[-2:]
+            if debug:
+                print >>sys.stderr,"ipDate = %s" % ipData
             break
     if ipData:
-        print "%s\t\t%s\t\t%s" % (domCurr.name(),ipData[-1],ipData[-2])    
+        print "%s\t\t%s\t\t%s" % (domCurr.name(),ipData[0],ipData[1])
