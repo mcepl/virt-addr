d297ec2e54cb0cd29eca5208566bbbb5d0bd6f41
Matěj Cepl <mcepl@redhat.com>
2014-08-12 15:26:40 +0200

    Cleanup and PEP8ization.

--- virt-addr.py
+++ virt-addr.py
@@ -1,35 +1,34 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""
-Requires fping.
-"""
-
+from __future__ import print_function
 try:
-	from lxml import etree
+    from lxml import etree
 except ImportError:
-	import xml.etree.cElementTree as etree
+    import xml.etree.cElementTree as etree
 import libvirt
-import sys,subprocess,os
-import StringIO
+import sys
+import subprocess
 
 debug = False
 parser = etree.XMLParser()
 
 conn = libvirt.openReadOnly(None)
-if conn == None:
-    print 'Failed to open connection to the hypervisor'
+if conn is None:
+    print('Failed to open connection to the hypervisor')
     sys.exit(1)
 
-domain=conn.lookupByName(sys.argv[1])
+domain = conn.lookupByName(sys.argv[1])
 desc = etree.fromstring(domain.XMLDesc(0))
-macAddr = desc.find("devices/interface[@type='network']/mac").attrib["address"].lower().strip()
+macAddr = desc.find(
+    "devices/interface[@type='network']/mac").attrib["address"].lower().strip()
 if debug:
-    print >>sys.stderr,"XMLDesc = %s" % macAddr
+    print("XMLDesc = %s" % macAddr, file=sys.stderr)
 
-output = subprocess.Popen(["arp", "-n"], stdout=subprocess.PIPE).communicate()[0]
+output = subprocess.Popen(["arp", "-n"],
+                          stdout=subprocess.PIPE).communicate()[0]
 lines = [line.split() for line in output.split("\n")[1:]]
 if debug:
-	print lines
+    print(lines)
 IPaddr = [line[0] for line in lines if (line and (line[2] == macAddr))]
 if IPaddr:
-	print IPaddr[0]
+    print(IPaddr[0])
