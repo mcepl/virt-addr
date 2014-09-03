40f2294f5d975f661bb3f437a70e5b00168a4981
Matěj Cepl <mcepl@redhat.com>
2014-09-03 07:58:33 +0200

    Cleanup ... don't use lxml.etree and use logging.

--- virt-addr.py
+++ virt-addr.py
@@ -1,34 +1,36 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from __future__ import print_function
-try:
-    from lxml import etree
-except ImportError:
-    import xml.etree.cElementTree as etree
-import libvirt
-import sys
-import subprocess
 
-debug = False
+import logging
+import subprocess
+import sys
+import xml.etree.cElementTree as etree
+
+import libvirt
+
+
+logging.basicConfig(format='%(levelname)s:%(funcName)s:%(message)s',
+                    level=logging.INFO)
+
 parser = etree.XMLParser()
 
 conn = libvirt.openReadOnly(None)
 if conn is None:
-    print('Failed to open connection to the hypervisor')
+    logging.error('Failed to open connection to the hypervisor')
     sys.exit(1)
 
 domain = conn.lookupByName(sys.argv[1])
 desc = etree.fromstring(domain.XMLDesc(0))
 macAddr = desc.find(
     "devices/interface[@type='network']/mac").attrib["address"].lower().strip()
-if debug:
-    print("XMLDesc = %s" % macAddr, file=sys.stderr)
+logging.debug("XMLDesc = %s", macAddr)
 
 output = subprocess.Popen(["arp", "-n"],
                           stdout=subprocess.PIPE).communicate()[0]
 lines = [line.split() for line in output.split("\n")[1:]]
-if debug:
-    print(lines)
+logging.debug(lines)
+
 IPaddr = [line[0] for line in lines if (line and (line[2] == macAddr))]
 if IPaddr:
     print(IPaddr[0])
